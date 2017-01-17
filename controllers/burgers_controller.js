e2092f7a1cc74ca8acb743df4dfc47ef067ec60c
rbuccat <richbuccat@yahoo.com>
2017-01-16 17:57:51 -0800

    css styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ router.get("/", function(req, res) {
 });
 
 router.get("/burgers", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/burgers", function(req, res) {
 });
 
 router.post("/burgers/create", function(req, res) {
-  burger.create([
+  burger.insertOne([
     "burger_name", "devoured" 
   ], [
     req.body.burger_name, req.body.devoured
@@ -31,7 +31,7 @@ router.put("/burgers/update/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
+  burger.updateOne({
     devoured: req.body.devoured
   }, condition, function() {
     res.redirect("/burgers");
